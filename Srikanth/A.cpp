029d19077311d59bccbedd77d275edf62a381626
Srikanth Iyengar <ksrikanth3012@gmail.com>
2022-02-24 23:30:50 +0530

    Srikanth

--- Srikanth/A.cpp
+++ Srikanth/A.cpp
@@ -2,8 +2,33 @@
 #include "score.cpp"
 using namespace std;
 
+struct person {
+    string name;
+    int skills;
+    vector<pair<string, int>> sk_set;
+    person() {
+        cin >> name;
+        cin >> skills;
+        for(int i =0 ; i < skills ; i++) {
+            string tmp;
+            int level;
+            cin >> tmp >> level;
+            sk_set.push_back({tmp, level});
+        }
+    }
+};
+
+void debug(person p) {
+
+}
+ 
 void solve() {
-    
+    int c, p; cin >> c >> p;
+    vector<person> contri;
+    for(int i = 0 ; i < c ; i++){
+        contri.push_back(person());
+    }
+
 }
 
 int main() {
