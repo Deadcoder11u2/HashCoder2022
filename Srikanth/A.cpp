2fd7e93f3ba5aa777c17c07752854d0734c82fb3
Srikanth Iyengar <ksrikanth3012@gmail.com>
2022-02-24 22:23:06 +0530

    s

--- Srikanth/A.cpp
+++ Srikanth/A.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
 }
